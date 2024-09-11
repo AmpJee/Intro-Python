ea71a003f9d281c65003a4bd2d1967b18242a0a8
AmpJee <amp.2549@gmail.com>
2024-09-11 17:45:18 +0700

    first day lecture

--- python-10-09.py
+++ python-10-09.py
@@ -1,1 +1,9 @@
+"""""
 print("hello world!")
+print("hi", "hey", "123", sep=",")
+print("hi", "hey", "123", sep="\n")
+name = input()
+print(name)
+"""""
+print(type(0.0))
+print(isinstance(-1,int))
