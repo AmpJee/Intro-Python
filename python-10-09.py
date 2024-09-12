d670ec5d5d452628989fdb3eee61cda7fadf9214
AmpJee <amp.2549@gmail.com>
2024-09-12 16:58:10 +0700

    lecture

--- python-10-09.py
+++ python-10-09.py
@@ -1,9 +1,5 @@
-"""""
 print("hello world!")
 print("hi", "hey", "123", sep=",")
 print("hi", "hey", "123", sep="\n")
-name = input()
-print(name)
-"""""
 print(type(0.0))
 print(isinstance(-1,int))
--- python-11-09.py
+++ python-11-09.py
@@ -2,5 +2,12 @@
 name = "G"
 age = 18
 date_of_birrth = 28
-text = input()
-print(text)
+# text = input()
+# print(text)
+a = int("1")
+b = float(2)
+c = str(True)
+d = bool(3)
+e = 7//2
+f = 5%2
+a+=20
