ee1d2aeaac175826c3606b25a508d34eee7b700e
AmpJee <amp.2549@gmail.com>
2024-09-19 19:47:55 +0700

    19.09

---
+++ lecture/python-19-09.py
@@ -0,0 +1,20 @@
+empty_list = []
+empty_list = list()
+my_list = [1, 2, "ALICE", True]
+print(len(my_list))
+print(my_list[3:0:-2])
+print(my_list[::-1])
+my_list.append(4)
+print(my_list)
+my_list.insert(2, 55)
+print(my_list)
+my_list.extend([2, 5, 9, 33])
+print(my_list)
+my_list += [2, 4, 7, 47]
+print(my_list)
+my_list.pop()
+print(my_list)
+my_list.pop(5)
+print(my_list)
+my_list.remove(5)
+print(my_list)
